7838fd1059034fbb5482c04bd9f4faded93b805d
Cong Liu <cilazi@qq.com>
2015-11-17 00:23:31 -0800

    Cup Moves

--- Assets/CupLogic.cs
+++ Assets/CupLogic.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CupLogic : MonoBehaviour
 {
-
+    public Transform m_GameLogic;
     // Use this for initialization
     void Start()
     {
@@ -13,27 +13,37 @@ public class CupLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if(!arrived)
+            Move();
     }
 
-    private Transform dest;
-    private bool arrived;
-    private float m_Speed = 50f;
 
-    public void MoveTo(Transform destination)
+    private Vector3 dest;
+    private bool arrived=true;
+    private float m_Speed = 10f;
+    private Vector3 direct;
+
+    public void MoveTo(Vector3 pos)
     {
         arrived = false;
-        dest = destination;
+        dest = new Vector3(pos.x, pos.y, pos.z);
+        direct = dest - transform.position;
     }
 
     void Move()
     {
-        if (transform == dest)
+        if (arrived)
+            return;
+
+        Vector3 differ = transform.position - dest;
+
+        if (differ.magnitude<0.1)
         {
             arrived = true;
+            transform.position = dest;
             return;
         }
-        Vector3 direct = dest.position - transform.position;
-        transform.Translate(direct.normalized * Time.deltaTime * m_Speed);
+
+        transform.Translate(direct.normalized * Time.deltaTime * m_Speed, Space.World);
     }
 }
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -1,23 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameLogic : MonoBehaviour {
 
     public Transform[] cups;
+    public Transform showNumbers;
 
 	// Use this for initialization
 	void Start () {
-	
+
+        showStart = Time.time;
+        BuildSequence();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (!b_Ishide)
+            ShowNumberAndHide();
+        else
+        {
+            CheckDelay();
+            //TestSwap();
+        }
+    }
+
+
+
+    private float showStart;
+    private bool b_Ishide = false;
+
+    void ShowNumberAndHide()
+    {
+        if (Time.time - showStart > 2)
+        {
+            b_Ishide = true;
+            showNumbers.gameObject.SetActive(false);
+            lastFinished = Time.time;
+        }
+    }
 
     void swap(int i, int j)
     {
-        //cups[i].gameObject.GetComponent<CupLogic>();
+        cups[i].gameObject.GetComponent<CupLogic>().MoveTo(cups[j].position);
+        cups[j].gameObject.GetComponent<CupLogic>().MoveTo(cups[i].position);
     }
 
+    void TestSwap()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            swap(0, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            swap(0, 2);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            swap(1, 2);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            bool active = cups[0].gameObject.activeSelf;
+            cups[0].gameObject.SetActive(!active);
+        }
+    }
+
+    private List<int> Sequence=new List<int>();
+
+    void BuildSequence()
+    {
+        Sequence.Add(1);
+        Sequence.Add(2);
+        Sequence.Add(3);
+    }
+
+    private float lastFinished;
+
+
+
+    private void CheckDelay()
+    {
+        if (Time.time - lastFinished > delay)
+        {
+            lastFinished = Time.time;
+            UseSequence();
+        }
+    }
+
+    private bool b_NeedToSwap = false;
+    private float delay = 1f;
+
+
+
+    public void UseSequence()
+    {
+
+        if (Sequence.Count > 0)
+        {
+            lastFinished = Time.time;
+            if (Sequence[0] == 1)
+                swap(0, 1);
+            else if (Sequence[0] == 2)
+                swap(0, 2);
+            else
+                swap(1, 2);
+            Sequence.RemoveAt(0);
+        }
+        b_NeedToSwap = false;
+    }
 }
